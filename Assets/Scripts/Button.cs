95274fe056df0c6dec1f1b6227c5f2f95ca2b7ab
AIDAN O'DONNELL <196555449+SomeDerg-fr@users.noreply.github.com>
2025-10-02 13:39:58 +0200

    Begun Steam Integration
    - Added in packages for steam
    - Currently reworking the main lobby to allow for this new set up
    - Made manager scripts for the new scenes (got code from tutorial)

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,21 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
-    public GameObject buttonObject;
-    // Start is called before the first frame update
-    void Start()
+    public void loadGame()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit raycastHit;
-        if (Physics.Raycast(ray, out raycastHit) && raycastHit.collider.gameObject == buttonObject && Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("Game");
-        }
+        Debug.Log("AAAAAAAAAAA");
+        //SceneManager.LoadScene("Game");
+        //SceneManager.LoadScene("Game", LoadSceneMode.Additive);
     }
 }
